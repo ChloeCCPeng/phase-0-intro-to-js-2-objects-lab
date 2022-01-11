161bab7cab45f7750ea9419f490b8daf2508a293
Chloe Peng <chloepeng331@icloud.com>
2022-01-11 15:21:32 -0500

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "chloe",
+    streetAddress: "1234 abc rd.",
+};
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    //employee[name] = streetAddress;
+    const newEmployee = { ...employee};
+    newEmployee.streetAddress = "11 Broadway";
+    newEmployee.name = "Sam";
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    const newEmployee = employee;
+    newEmployee.streetAddress = "12 Broadway";
+    newEmployee.name = "Sam";
+    return newEmployee;
+};
+
+function deleteFromEmployeeByKey(newEmployee, name){
+    const newEmployee2 = { ...newEmployee};
+    delete newEmployee2.name;
+    return newEmployee2;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
