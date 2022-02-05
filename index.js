eae20b0600ffa59263003aeb70ec6cfbba12b44c
Chloe Peng <chloepeng331@icloud.com>
2022-02-05 17:15:03 -0500

    commit tree 01ffcbbb41fdc2936f037704bb523f3145dd7e7c

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-// Write your solution in this file!
 const employee = {
-    name: "chloe",
-    streetAddress: "1234 abc rd.",
+    name: 'Sam',
+    streetAddress: '11 Broadway',
 };
-console.log(employee);
 
-function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
-    //employee[name] = streetAddress;
-    const newEmployee = { ...employee};
-    newEmployee.streetAddress = "11 Broadway";
-    newEmployee.name = "Sam";
+// const employee = {
+//     name: "chloe",
+//     streetAddress: "1234 abc rd.",
+// };
+// console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee, [key]: value};
     return newEmployee;
-};
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
-    const newEmployee = employee;
-    newEmployee.streetAddress = "12 Broadway";
-    newEmployee.name = "Sam";
-    return newEmployee;
-};
-
-function deleteFromEmployeeByKey(newEmployee, name){
-    const newEmployee2 = { ...newEmployee};
-    delete newEmployee2.name;
-    return newEmployee2;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key){
+// function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+//     const newEmployee = { ...employee};
+//     newEmployee.streetAddress = "11 Broadway";
+//     newEmployee.name = "Sam";
+//     return newEmployee;
+// };
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee){
+    employee.streetAddress = '12 Broadway';
+    return employee;
+};
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+//     const newEmployee = employee;
+//     newEmployee.streetAddress = "12 Broadway";
+//     newEmployee.name = "Sam";
+//     return newEmployee;
+// };
+
+function deleteFromEmployeeByKey (employee, key, value){
+    const newEmployee  = {...employee, [key]: value};
+    delete newEmployee.key;
+    return newEmployee;
+}
+
+// function deleteFromEmployeeByKey(newEmployee, name){
+//     const newEmployee2 = { ...newEmployee};
+//     delete newEmployee2.name;
+//     return newEmployee2;
+// }
+
+function destructivelyDeleteFromEmployeeByKey(employee){
     delete employee.name;
     return employee;
 }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee.name;
+//     return employee;
+// }
